e7068c39f2daa7764eed9ca6fc640478ed4ad929
Eddy Mhalli <eddy.mhalli@gmail.com>
2024-11-07 23:40:20 +0100

    feat(fix): correct Welcome component className

--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -13,7 +13,7 @@ export const Welcome: React.FC<ChatWelcome> = ({ onRegister }) => {
   const userRegistering = useRef(false);
 
   return (
-    <div className="chat-container">
+    <div className="chat">
       <h2>Welcome to this free Chat with AI</h2>
       <p>Please enter your name</p>
       {userRegistering.current === true ? (
